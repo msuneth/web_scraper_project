e7cc35c887fadfe4aafbd5c7b9dab403b1de23fd
msuneth <msuneth@gmail.com>
2024-03-22 08:01:35 +0530

    capture different elements using soap object

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ with open("website.html",encoding="utf-8") as file:
 soup = BeautifulSoup(content,'html.parser')
 print(soup.title)
 print(soup.title.string)
+print(soup.prettify())
+print(soup.a)
+print(soup.p)
+print(soup.li)
+
+all_anchor_tag = soup.findAll(name="a")
+print(all_anchor_tag)
+
+for tag in all_anchor_tag:
+    print(tag.getText())
+    print(tag.get("href"))
+
+heading = soup.find(name="h1",id="name")
+print(heading)
+
+section_heading = soup.find(name="h3",class_="heading")
+print(section_heading)
