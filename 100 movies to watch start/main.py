440b9dd08563e628dd6841c271fc67299aedcfa0
msuneth <msuneth@gmail.com>
2024-04-01 05:19:52 +0530

    100 movies watch-get first movie

--- 100 movies to watch start/main.py
+++ 100 movies to watch start/main.py
@@ -7,5 +7,8 @@ URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 # Write your code below this line 👇
 response = requests.get(URL)
 web_page = response.text
-print(web_page)
+soup = BeautifulSoup(web_page,'html.parser')
+#article_span_tag = soup.find_all('span', class_='titleline')
+movie_titles = soup.find('h3', class_='listicleItem_listicle-item__title__BfenH')
+print(movie_titles.getText())
 
