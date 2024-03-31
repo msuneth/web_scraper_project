43238e8661bc65742cbde7cd4cea279788160953
msuneth <msuneth@gmail.com>
2024-04-01 05:24:25 +0530

    100 movies watch-write movies to file

--- 100 movies to watch start/main.py
+++ 100 movies to watch start/main.py
@@ -10,6 +10,8 @@ web_page = response.text
 soup = BeautifulSoup(web_page,'html.parser')
 #article_span_tag = soup.find_all('span', class_='titleline')
 movie_titles = soup.find_all('h3', class_='listicleItem_listicle-item__title__BfenH')
-for movie in movie_titles:
-    print(movie.getText())
+with open("movie_list.txt","w") as file:
+    for movie in movie_titles:
+        file.writelines(movie.getText()+"\n")
+        print(movie.getText())
 
