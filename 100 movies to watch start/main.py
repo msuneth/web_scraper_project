f39bfcfa9021f58e7f1e08ba4e09843dfac45e5a
msuneth <msuneth@gmail.com>
2024-04-05 07:13:37 +0530

    100 movies watch-write sorted movies to file

--- 100 movies to watch start/main.py
+++ 100 movies to watch start/main.py
@@ -10,8 +10,9 @@ web_page = response.text
 soup = BeautifulSoup(web_page,'html.parser')
 #article_span_tag = soup.find_all('span', class_='titleline')
 movie_titles = soup.find_all('h3', class_='listicleItem_listicle-item__title__BfenH')
+movie_titles_sorted = movie_titles[::-1]
 with open("movie_list.txt","w") as file:
-    for movie in movie_titles:
+    for movie in movie_titles_sorted:
         file.writelines(movie.getText()+"\n")
         print(movie.getText())
 
